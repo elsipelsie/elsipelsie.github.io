7fa975e8939dfdcc8d2cf9374abcb73b27301957
Elsa <elsatuomi@hotmail.com>
2019-04-03 19:55:40 +0100

    CodeFirst:Girls website
    Project done by Elsa Tuomi and Rochelle Martins

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+
+$('.goudaBtnToWine').click(function() {
+  window.location.href='wine.html';
+});
+
+$('.wineBtnToCheese').click(function() {
+  window.location.href='cheese.html';
+});
+
+$("#submit-button").click(function() {
+  alert("That's Brie-lliant! Thanks for signing up!");
+});
